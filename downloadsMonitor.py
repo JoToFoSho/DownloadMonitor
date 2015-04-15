ed386160b79209e5ce2554d6987deba3831735c1
BRILLIGENT\john.tobe <john@tobe.name>
2015-04-15 12:55:54 -0400

    Fixing indents and other style errors found by pycharm

--- downloadsMonitor.py
+++ downloadsMonitor.py
@@ -1,22 +1,22 @@
 import sys
-import time
 import os.path
-import shutil
 import re
 import signal
 import urllib2
- 
+
 log_file = open("/var/log/downloadsMonitor.log", "a")
 processedFileStr = "/volume1/Downloads/processed.txt"
 errorFileStr = "/volume1/Downloads/error.txt"
-##log_file = open("/Users/tobeswsu/Desktop/downloadsMonitor.log", "a")
-##processedFileStr = "/Users/tobeswsu/Desktop/processed.txt"
-##errorFileStr = "/Users/tobeswsu/Desktop/error.txt"
-     
+# log_file = open("/Users/tobeswsu/Desktop/downloadsMonitor.log", "a")
+# processedFileStr = "/Users/tobeswsu/Desktop/processed.txt"
+# errorFileStr = "/Users/tobeswsu/Desktop/error.txt"
+
+
 def log(text):
     log_file.write(text + "\n")
     log_file.flush()
-     
+
+
 '''pid = str(os.getpid())
 pid_file='/var/run/downloadsMonitor.pid'
 
@@ -25,24 +25,26 @@ if os.path.isfile(pid_file):
     sys.exit()
 else:
     file(pid_file, 'w').write(pid)'''
-    
+
 log("Starting")
 
+
 def signal_handler(signal, frame):
-    #os.unlink(pid_file)
+    # os.unlink(pid_file)
     log("Exiting")
     sys.exit(0)
- 
+
+
 signal.signal(signal.SIGTERM, signal_handler)
- 
+
 allowed_exts = ["mp4", "avi", "mov", "mkv", "m4v"]
 
-##tvDir = "/Users/tobeswsu/Desktop/TV Shows/"
-##kidTvDir = "/Users/tobeswsu/Desktop/Kid Shows/"
+# tvDir = "/Users/tobeswsu/Desktop/TV Shows/"
+# kidTvDir = "/Users/tobeswsu/Desktop/Kid Shows/"
 tvDir = "/volume1/TV Shows/"
 kidTvDir = "/volume1/Kid Shows/"
- 
- 
+
+
 class DownloadMonitor():
     def __init__(self):
         self.processed_files = set()
@@ -53,8 +55,8 @@ class DownloadMonitor():
         else:
             self.processed_files = set()
         self.processing_file = open(processedFileStr, "a")
-        
-        self.error_files= set()
+
+        self.error_files = set()
         if os.path.isfile(errorFileStr):
             tmp = open(errorFileStr, "r")
             self.error_files.update(tmp.read().splitlines())
@@ -64,53 +66,53 @@ class DownloadMonitor():
         self.error_file = open(errorFileStr, "a")
         self.addedTVShow = False
         self.addedKidShow = False
-         
+
     def scanDirectory(self, directory):
         log("Scanning %s" % (directory))
         for root, dirs, filenames in os.walk(directory):
             for filename in filenames:
                 self.process_file(directory, filename)
             for folder in dirs:
-                log("folder path = %s" % os.path.join(directory,folder))
-                self.scanDirectory(os.path.join(directory,folder))
-         
+                log("folder path = %s" % os.path.join(directory, folder))
+                self.scanDirectory(os.path.join(directory, folder))
+
     def process_file(self, path, filename):
+
         if filename in self.processed_files:
             return True
 
         self.processed_files.add(filename)
-        self.processing_file.write(filename + "\n");
-        
+        self.processing_file.write(filename + "\n")
+
         if not self.is_allowed_path(filename):
-            pathname = os.path.join(path, filename)
             return False
-        
-		parentDir = os.path.basename(path)
-        names = [filename,parentDir]
+
+        parentDir = os.path.basename(path)
+        names = [filename, parentDir]
         temp = ""
-        for name in names: 
-			temp = re.findall('(.*)\.[S|s]?(\d+)[x|e|E](\d+)(.*)', name)
-			if len(temp) <= 0:
-				temp = re.findall('(.*) [S|s]?(\d+)[x|e|E](\d+)(.*)', name)
-			if len(temp) <= 0:
-				temp = re.findall('(.*)\.(\d{4}?).(\d{2}\.\d{2}?)(.*)', name)
-			if len(temp) <= 0:
-				temp = re.findall('(.*)(Season?).(Episode?)(.*)', name)
-			if len(temp) <= 0:
-				temp = re.findall('(.*)(\d+?)x(\d+?).(.*)', name)
-			if len(temp) > 0:
-			    if name != filename:
-				    newName = name + os.path.splitext(filename)[1]
-					log("changing filename {0} to {1}".format(filename,newName))
-					oldPath = os.path.join(path,filename)
-					newPath = os.path.join(path,newName)
-					os.link(oldPath, newPath)
-					filename = newName
-				break;
+        for name in names:
+            temp = re.findall('(.*)\.[S|s]?(\d+)[x|e|E](\d+)(.*)', name)
+            if len(temp) <= 0:
+                temp = re.findall('(.*) [S|s]?(\d+)[x|e|E](\d+)(.*)', name)
+            if len(temp) <= 0:
+                temp = re.findall('(.*)\.(\d{4}?).(\d{2}\.\d{2}?)(.*)', name)
+            if len(temp) <= 0:
+                temp = re.findall('(.*)(Season?).(Episode?)(.*)', name)
+            if len(temp) <= 0:
+                temp = re.findall('(.*)(\d+?)x(\d+?).(.*)', name)
+            if len(temp) > 0:
+                if name != filename:
+                    new_name = name + os.path.splitext(filename)[1]
+                    log("changing filename {0} to {1}".format(filename, new_name))
+                    oldPath = os.path.join(path, filename)
+                    newPath = os.path.join(path, new_name)
+                    os.link(oldPath, newPath)
+                    filename = new_name
+                break
         if len(temp) <= 0:
             log("Error parsing filename %s" % filename)
             self.error_files.add(filename)
-            self.error_file.write(filename + "\n");
+            self.error_file.write(filename + "\n")
             return False
         log("temp = %s" % temp)
 
@@ -118,56 +120,55 @@ class DownloadMonitor():
             log("Partial error parsing filename %s" % filename)
             log("Parsed tuple: %s" % temp[0])
         show = temp[0][0]
-        show = show.replace(".","-")
-        show = show.replace(" ","-")
-        show = show.replace("_","-")
+        show = show.replace(".", "-")
+        show = show.replace(" ", "-")
+        show = show.replace("_", "-")
         show = show.lower()
         if show == 'american-dad' and len(temp[0]) > 3:
-            src = os.path.join(path,filename);
-            season = int(temp[0][1]);
-            season += 1;
-            season = "{0:0>2}".format(season);
-            filename = filename.replace(temp[0][1],season,1);
-            dest = os.path.join(path,filename);
+            src = os.path.join(path, filename)
+            season = int(temp[0][1])
+            season += 1
+            season = "{0:0>2}".format(season)
+            filename = filename.replace(temp[0][1], season, 1)
+            dest = os.path.join(path, filename)
             try:
-                os.link(src,dest);
+                os.link(src, dest)
                 self.processed_files.add(filename)
-                self.processing_file.write(filename + "\n");
+                self.processing_file.write(filename + "\n")
             except IOError, e:
-                log("Unable to fix season for %s" % e);
-                self.error_files.add(filename);
-                self.error_file.write(filename + "\n");
+                log("Unable to fix season for %s" % e)
+                self.error_files.add(filename)
+                self.error_file.write(filename + "\n")
         dest_path = ""
         for name in os.listdir(tvDir):
             if name.lower() == show:
-                dest_path = os.path.join(tvDir,name)
+                dest_path = os.path.join(tvDir, name)
                 self.addedTVShow = True
                 break
         if dest_path == "":
             for name in os.listdir(kidTvDir):
                 if name.lower() == show:
-                    dest_path = os.path.join(kidTvDir,name)
+                    dest_path = os.path.join(kidTvDir, name)
                     self.addedTVShow = True
                     break
 
         if dest_path == "":
             log("Missing destination directory for show: %s" % show)
             self.error_files.add(filename)
-            self.error_file.write(filename + "\n");
+            self.error_file.write(filename + "\n")
             return False
         log("Moving to directory %s" % dest_path)
-        src = os.path.join(path,filename)
+        src = os.path.join(path, filename)
         try:
-            os.link(src,dest_path)
+            os.link(src, dest_path)
         except IOError, e:
-            log("Unable to copy file. %s" % e )
+            log("Unable to copy file. %s" % e)
             self.error_files.add(filename)
-            self.error_file.write(filename + "\n");
+            self.error_file.write(filename + "\n")
 
         return True
 
-         
-             
+
     def is_allowed_path(self, filename):
         filestr = filename.lower()
         ext = os.path.splitext(filename)[1][1:].lower()
@@ -176,11 +177,12 @@ class DownloadMonitor():
         if filestr.find('sample') >= 0:
             return False
         return True
- 
+
+
 watcher = DownloadMonitor()
 watcher.scanDirectory("/volume1/Downloads/")
 if watcher.addedTVShow:
     urllib2.urlopen("http://192.168.2.99:32400/library/sections/2/refresh")
 if watcher.addedKidShow:
     urllib2.urlopen("http://192.168.2.99:32400/library/sections/3/refresh")
-##watcher.scanDirectory("/Users/tobeswsu/Desktop/")
+# watcher.scanDirectory("/Users/tobeswsu/Desktop/")
